3ee11a626d747832d4c4bff7fb0df30f7db0de03
Sean <skelstar@gmail.com>
2020-05-19 08:49:53 +1200

    Got mqtt and wifi going.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,15 @@
 #include <Arduino.h>
 #include <elapsedMillis.h>
 
+#include <WiFi.h>
+#include <wificonfig.h>
+#include <PubSubClient.h>
+
+// #include <DNSServer.h>
+// #include <WebServer.h>
+// #include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
+
+//---------------------------------------------------------------
 #include <FastLED.h>
 
 #define NUM_LEDS 1
@@ -29,10 +38,141 @@ void setLed(CRGB colour)
 }
 //---------------------------------------------------------------
 
+WiFiClient client;
+PubSubClient mqttclient(client);
+
+void connectToWifi()
+{
+  int status = 0;
+
+  while (status != WL_CONNECTED)
+  {
+    Serial.printf("Attempting to connect to WPA SSID: %s\n", ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+    // wait 10 seconds for connection:
+    if (status != WL_CONNECTED)
+    {
+      delay(3000);
+    }
+    else
+    {
+      DEBUG("Connected to wifi!");
+    }
+  }
+}
+
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+  payload[length] = '\0';
+
+  char *command;
+  char *p = (char *)payload;
+
+  if (strcmp(topic, "/device/atom-lite-1/test") == 0)
+  {
+    DEBUGVAL(topic, (char *)payload);
+  }
+  //   if (strcmp(topic, TOPIC_LED_COMMAND) == 0)
+  //   {
+  //     while ((command = strtok_r(p, "$", &p)) != NULL)
+  //     {
+  //       if (strcmp(command, "LED") == 0)
+  //       {
+  //         char *colour = strtok_r(p, "$", &p);
+  //         if (strcmp(colour, "RED") == 0)
+  //         {
+  //           fireButtonLed[0] = CRGB::Red;
+  //         }
+  //         else if (strcmp(colour, "GREEN") == 0)
+  //         {
+  //           fireButtonLed[0] = CRGB::Green;
+  //         }
+  //         else if (strcmp(colour, "BLUE") == 0)
+  //         {
+  //           fireButtonLed[0] = CRGB::Blue;
+  //         }
+  //         else if (strcmp(colour, "OFF") == 0)
+  //         {
+  //           fireButtonLed[0] = CRGB::Black;
+  //         }
+  //         FastLED.show();
+  //       }
+  //       else
+  //       {
+  //         Serial.printf("command?: %s \n", command);
+  //       }
+  //     }
+  //   }
+  //   else if (strcmp(topic, TOPIC_TIMESTAMP_10S) == 0)
+  //   {
+  //     sinceHeardFromMqtt = 0;
+  //   }
+}
+
+void subscribe(char *root, char *subtopic)
+{
+  char topic[40];
+  strcpy(topic, root);
+  strcat(topic, "/");
+  strcat(topic, subtopic);
+
+  mqttclient.subscribe(topic);
+  DEBUGVAL(topic);
+}
+
+void connectToMqtt()
+{
+  DEBUG("Connecting to MQTT...");
+  mqttclient.setServer("192.168.1.105", 1883);
+  mqttclient.setCallback(mqttCallback);
+  int attempts = 0;
+  while (!mqttclient.connected())
+  {
+#define MQTT_NODENAME "/device/atom-lite-1"
+
+    // Attempt to connect
+    mqttclient.connect(MQTT_NODENAME, "skelstar", pass);
+    if (attempts++ > 5)
+    {
+      Serial.printf("Couldn't connect to MQTT!\n");
+      delay(1000);
+    }
+  }
+
+  subscribe(MQTT_NODENAME, "test");
+
+  // mqttclient.subscribe(TOPIC_LED_COMMAND);
+  // mqttclient.subscribe(TOPIC_TIMESTAMP_10S);
+  Serial.printf("Connected to MQTT! \n");
+
+  // mqttPublish(TOPIC_ONLINE, "ONLINE");
+
+  Serial.printf("ip: %s \n", WiFi.localIP());
+}
+
+//---------------------------------------------------------------
+
 void setup()
 {
   Serial.begin(115200);
   DEBUG("Ready");
+
+  connectToWifi();
+  connectToMqtt();
+
+  // WiFiManager wifiManager;
+  // //wifiManager.resetSettings();
+  // //set custom ip for portal
+  // //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+  // //fetches ssid and pass from eeprom and tries to connect
+  // //if it does not connect it starts an access point with the specified name
+  // //here  "AutoConnectAP"
+  // //and goes into a blocking loop awaiting configuration
+  // wifiManager.autoConnect("m5-atom-lite");
+  // //if you get here you have connected to the WiFi
+  // Serial.println("connected...yeey :)");
 
   // button.setReleasedHandler(toggleMic);
   button.setDoubleClickHandler([](Button2 &btn) {
@@ -54,4 +194,6 @@ void setup()
 void loop()
 {
   button.loop();
+
+  mqttclient.loop();
 }
